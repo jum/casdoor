4811b10d821fbca2f850d88c53df29cab8ef4e15
Yang Luo <hsluoyz@gmail.com>
2026-04-14 01:47:16 +0800

    feat: improve MFA page UI

--- web/src/auth/mfa/MfaVerifyPushForm.js
+++ web/src/auth/mfa/MfaVerifyPushForm.js
@@ -8,7 +8,7 @@ export const MfaVerifyPushForm = ({mfaProps, application, onFinish, method, user
   return (
     <Form
       form={form}
-      style={{width: "300px"}}
+      style={{width: "300px", margin: "0 auto"}}
       onFinish={onFinish}
       initialValues={{
         enableMfaRemember: false,
