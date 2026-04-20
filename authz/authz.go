7816c29ae33403eefea5b5df4287455a82702367
Yang Luo <hsluoyz@gmail.com>
2026-04-21 00:16:53 +0800

    fix: improve update-payment API

--- authz/authz.go
+++ authz/authz.go
@@ -76,7 +76,6 @@ p, *, *, GET, /api/get-order, *, *
 p, *, *, GET, /api/get-orders, *, *
 p, *, *, GET, /api/get-user-orders, *, *
 p, *, *, GET, /api/get-payment, *, *
-p, *, *, POST, /api/update-payment, *, *
 p, *, *, POST, /api/invoice-payment, *, *
 p, *, *, POST, /api/notify-payment, *, *
 p, *, *, POST, /api/place-order, *, *
